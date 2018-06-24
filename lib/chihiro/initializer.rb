7d5527d8325ce67097cfe1cfb311349077675b9a
Shu Hui <dreamlock0630@gmail.com>
2018-06-24 22:34:01 +1000

    update 1.4

--- lib/chihiro/initializer.rb
+++ lib/chihiro/initializer.rb
@@ -7,9 +7,11 @@ module Chihiro
         config.lograge.enabled = true
         config.lograge.base_controller_class = "ActionController::API"
         config.lograge.ignore_actions = ["HealthCheckController#index"]
-        logger = ActiveSupport::Logger.new(STDOUT)
-        logger.formatter = config.log_formatter
-        config.logger = logger
+        if ENV["RAILS_LOG_TO_STDOUT"].present?
+          logger = ActiveSupport::Logger.new(STDOUT)
+          logger.formatter = config.log_formatter
+          config.logger = logger
+        end
       end
     end
   end
